a8b7fc25f2d4c001b840d483b6fc0c60397da0b8
oren amema <orenamema@gmail.com>
2020-01-25 22:38:22 -0800

    Adding mockData

---
+++ mockData/mockData.js
@@ -0,0 +1,48 @@
+var db = require("../models");
+
+function createMock(){
+        var mockData = {
+        "Users": {"userName": "test_user",
+                "password": "test_password",
+                "createdAt": Date(), 
+                "updatedAt": Date()
+                },
+        "AvatarClasses": {"career": "test_career",
+                "socialClass": "test_socialClass",
+                "img": "test_img",
+                "createdAt": Date(), 
+                "updatedAt": Date()
+                },                
+        "Avatars": {"name": "test_name",
+                "age": 10,
+                "personality": "test_personality",
+                "backstory": "test_backstory",
+                "details": "test_details",
+                "AvatarClassesId": 10,
+                "createdAt": Date(), 
+                "updatedAt": Date()
+                },
+        // "Avatars": {"userName": "test_user",
+        //         "password": "test_password",
+        //         "createdAt": Date(), 
+        //         "updatedAt": Date()
+        //         },
+        // "AvatarClasses": {"userName": "test_user",
+        //         "password": "test_password",
+        //         "createdAt": Date(), 
+        //         "updatedAt": Date()
+        //         },
+        };
+
+
+        db.Users.create(mockData.Users).then(function(dbExample) {
+                console.log(`STORED Users!!`);
+        });
+        db.AvatarClasses.create(mockData.AvatarClasses).then(function(dbExample) {
+                console.log(`STORED AvatarClasses!!`);
+        });
+        db.Avatars.create(mockData.Avatars).then(function(dbExample) {
+                console.log(`STORED Avatars!!`);
+        });
+}
+module.exports = createMock;
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var db = require("./models");
 var app = express();
+var createMock = require("./mockData/mockData.js");
 var PORT = process.env.PORT || 8080;
 var exphbs = require("express-handlebars");
-
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
@@ -17,8 +17,16 @@ app.use(express.static("public"));
 require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
+// sample data
+var sample_user = {"userName": "test_user",
+                   "password": "test_password",
+                   "createdAt": Date(), 
+                   "updatedAt": Date()
+                   };
 
 db.sequelize.sync().then(function() {
+    console.log(Object.keys(db));
+    createMock();
     app.listen(PORT, function() {
       console.log("App listening on PORT " + PORT);
     });
