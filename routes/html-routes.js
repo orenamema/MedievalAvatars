aa969b1cee8c0fd075d9f60cd9aa890104da7ca8
KB <nkbains10@gmail.com>
2020-01-26 23:48:04 -0800

    added mock data for view all pages. Added view all pages dependancies files. View all page now loading images and info from database. filter options now working

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,4 +4,21 @@ module.exports = app => {
     app.get("/",(req,res) => {
         res.render("loginPage");
     });
+
+    app.get("/all",(req,res) => {
+        db.Avatars.findAll(
+            {
+                include: [
+                    {
+                        model: db.AvatarClasses, 
+                        as: "AvatarClasses",
+                        required: true
+                    }
+                ]
+            }).then(function(data){
+            res.render("viewAvatar", {avatars: data});
+        })
+    });
+
+
 }
