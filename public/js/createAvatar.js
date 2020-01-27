fbefb72c2b4fff49c800ee45660223e999258558
KB <nkbains10@gmail.com>
2020-01-27 14:14:15 -0800

    active img in carousel variable so it can be stored to database

--- public/js/createAvatar.js
+++ public/js/createAvatar.js
@@ -1,8 +1,16 @@
 $(function(){
+    var activeImage;
+
     $('.collapsible').collapsible();
     $('.carousel').carousel();
-
+    
     createCarousel();
+
+    $(".createPageList li").on("click", function(){
+        var btnValue = $(this).attr("data-name");
+        createCarousel(btnValue);
+    })
+
     function createCarousel(btnValue = "authority") {
         $('.carousel').html('');
         $("#carouselDiv").removeClass("initialized");
@@ -10,19 +18,18 @@ $(function(){
         $.get("/api/classes/" + btnValue, function(data){
             data.forEach(element => {
                 var imgName = element.img;
-                var appendItem = `<a class="carousel-item"><img src="${imgName}"></a>`;
+                var appendItem = `<a class="carousel-item" data-id="${element.id}"><img src="${imgName}"></a>`;
                 $(".carousel").append(appendItem);
          
             });
-            $('.carousel').carousel();
+            $('.carousel').carousel({
+                indicators: true,
+                onCycleTo: function(data) {
+                    activeImage = data.attr("data-id");
+               }
+            });
             $("#carouselDiv").addClass("initialized");
 
         });
-    }
-
-    $(".createPageList li").on("click", function(){
-        var btnValue = $(this).attr("data-name");
-        createCarousel(btnValue);
-    })
-
+    };
 });
