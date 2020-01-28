5de52c7725890dc49a8b8427c80105b54f298146
KB <nkbains10@gmail.com>
2020-01-28 09:40:56 -0800

    added careerDescription column to AvatarClasses table. Added all the careers and data to the mock data

--- public/js/viewAvatars.js
+++ public/js/viewAvatars.js
@@ -1,4 +1,9 @@
-$(document).ready(function(){
-    var imgHeights = [250, 300, 280, 320, 290, 260, 300, 280, 240, 260];
+$(function(){
+    // $('.fixed-action-btn').floatingActionButton();
+        var imgHeights = [250, 300, 280, 320, 290, 260, 300, 280, 240, 260];
 
-});
+        $("#menu").on("click", function(){
+            instances.open();
+        });
+})
+
