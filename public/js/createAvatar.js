9c2b013f9ba7f3d3682cf15ffd82c9d5b50888e8
Casey Moldavon <cmoldavon@gmail.com>
2020-01-29 18:12:17 -0800

    Adding Modals to floating btn (replacing tooltips)

--- public/js/createAvatar.js
+++ public/js/createAvatar.js
@@ -111,7 +111,16 @@ $(function(){
 
 
     // ========================= Slide button ==================================================
+    $('.tooltipped').tooltip();
+
     $(document).ready(function(){
-        $('.tooltipped').tooltip();
+        $('#modal1').modal();
+        $('#modal2').modal();
+        $('#modal3').modal();
+        $('#modal4').modal();
+        $('#modal5').modal();
+        $('#modal6').modal();
+        $('#modal7').modal();
     });
+
 });
